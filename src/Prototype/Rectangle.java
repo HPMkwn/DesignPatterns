925e31675338f58a34d46e9055b413ad8114cc44
harrymkwn <heratmakwana19@gmail.com>
2024-01-19 03:46:06 +0530

    Adding Prototype Design Pattern

---
+++ src/Prototype/Rectangle.java
@@ -0,0 +1,33 @@
+package Prototype;
+
+import org.w3c.dom.css.Rect;
+
+public class Rectangle extends Shape{
+
+    private int width;
+    private int height;
+
+    public Rectangle(int x, int y, String color, int width, int height) {
+        super(x, y, color);
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(Rectangle rectangle){
+        super(rectangle);
+        if(rectangle!=null){
+            this.height = rectangle.height;
+            this.width = rectangle.width;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+
+    public boolean equals(Object object2) {
+        if (!(object2 instanceof Rectangle shape2) || !super.equals(object2)) return false;
+        return shape2.width == width && shape2.height == height;
+    }
+}

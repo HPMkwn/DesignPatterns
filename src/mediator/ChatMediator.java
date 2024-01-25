5c154952819f5c8a081bd5a9d950349c9aafe0f2
harrymkwn <heratmakwana19@gmail.com>
2024-01-25 15:27:13 +0530

    Adding Mediator Design Pattern

---
+++ src/mediator/ChatMediator.java
@@ -0,0 +1,9 @@
+package mediator;
+
+public interface ChatMediator {
+
+    public void sendMessage(String message, User user);
+
+    public void addUser(User user);
+
+}
---
+++ src/mediator/Client.java
@@ -0,0 +1,17 @@
+package mediator;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ChatMediator mediator = new ConcreteMediator();
+        ConcreteUser user1 = new ConcreteUser(mediator,"Herat Makwana");
+        ConcreteUser user2 = new ConcreteUser(mediator,"Krup Koradia");
+        ConcreteUser user3 = new ConcreteUser(mediator, "OM Faldu");
+        ConcreteUser user4 = new ConcreteUser(mediator,"Chelsi Koradia");
+        ConcreteUser user5 = new ConcreteUser(mediator, "Janki Koradia");
+
+        user1.sendMessage("Hello From Herat Makwana");
+
+        user2.sendMessage("Hi Herat & EveryOne");
+    }
+}
